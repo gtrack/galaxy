4cf60f56333749ce4d67c72a23406649e0da5d27
Radmila Kompova <radmilko@ifi.uio.no>
2018-12-10 13:53:57 +0100

    gsuite support without sniffing

--- lib/galaxy/datatypes/interval.py
+++ lib/galaxy/datatypes/interval.py
@@ -1397,13 +1397,10 @@ class CustomTrack(Tabular):
 class GTrack(Interval):
     edam_format = "format_3164"
     file_ext = "gtrack"
-    # track_type = "LineTrack"
-    # data_sources = {"data": "bigwig", "index": "bigwig"}
 
     """Add metadata elements"""
-    # MetadataElement(name="comment_lines", default=0, desc="Number of comment lines",
-    #                 readonly=False, optional=True, no_value=0)
-
+    MetadataElement(name="comment_lines", default=0, desc="Number of comment lines",
+                     readonly=False, optional=True, no_value=0)
     MetadataElement(name="data_lines", default=0, desc="Number of data lines", readonly=True,
                     visible=False, optional=True, no_value=0)
     MetadataElement(name="header_lines", default=0, desc="Number of header lines",
@@ -1417,9 +1414,9 @@ class GTrack(Interval):
                     param=metadata.ColumnTypesParameter, readonly=True, visible=False, no_value=[])
     MetadataElement(name="column_names", default=[], desc="Column names", readonly=True,
                     visible=False, optional=True, no_value=[])
-    # MetadataElement(name="delimiter", default='\t', desc="Data delimiter", readonly=True,
-    #                 visible=False, optional=True, no_value=[])
-    #
+    MetadataElement(name="delimiter", default='\t', desc="Data delimiter", readonly=True,
+                    visible=False, optional=True, no_value=[])
+
     MetadataElement(name="chromCol", default=1, desc="Sequence id (chrom) column",
                     param=metadata.ColumnParameter)
     MetadataElement(name="startCol", default=2, desc="Start column",
@@ -1428,8 +1425,6 @@ class GTrack(Interval):
                     param=metadata.ColumnParameter)
     MetadataElement(name="strandCol", desc="Strand column",
                     param=metadata.ColumnParameter, optional=True, no_value=0)
-    # MetadataElement(name="nameCol", desc="Identifier column",
-    #                 param=metadata.ColumnParameter, optional=True, no_value=0)
 
     GTRACK_STD_COLUMN_TYPES = {
         'genome': 'str',
@@ -1580,7 +1575,7 @@ class GTrack(Interval):
                             'of {} columns'.format(
                 dataset.metadata.comment_lines,
                 dataset.metadata.header_lines,
-                dataset.metadata.bounding_regions,
+                #dataset.metadata.bounding_regions,
                 dataset.metadata.data_lines,
                 dataset.metadata.columns
             )
